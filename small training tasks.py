3234370dd0d9f13b167d2c875b010594e43ef307
Kate-Shcherbak <kate.shcherbak.work@gmail.com>
2024-01-31 11:51:00 +0500

    File small traning tasks added. 2 functions added.

---
+++ small training tasks.py
@@ -0,0 +1,8 @@
+#finction to reverse a list
+def reverse_list (list):
+    list.reverse()
+    print(list)
+
+#function to print list backwards 
+def print_list_backwards (list):
+    print(list[::-1])
